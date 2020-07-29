87304e2db969ba7659cb785e1b8b072ce519f901
Nicholas Diesslin <nicholasdiesslin@gmail.com>
2020-07-28 23:57:46 -0500

    Added rotating text in about section

--- src/components/partials/homepage/about.js
+++ src/components/partials/homepage/about.js
@@ -2,19 +2,41 @@
  * About component
  */
 
-import React from "react"
+import React, { Component } from 'react';
 
-const About = () => {
-  return (
-    <section class="background-color-black">
-      <div class="container container--medium">
-        <h2 class="about-text color-white margin-0 font-weight-normal">
-          My name is <strong id="nicholas">Nicholas&nbsp;Diesslin</strong>,<br/>
-          I’m a <strong>web developer / web designer / generalist</strong> from <strong>Minnesota</strong>. I like to build, make, and think about things!
-        </h2>
-      </div>
-    </section>
-  )
+const skillsetList = ['web developer', 'web designer', 'generalist', 'dev ops', 'email developer', 'information architect', 'usability developer'];
+
+class About extends Component {
+  constructor() {
+    super();
+    this.state = { textIdx: 0 };
+  }
+
+  componentDidMount() {
+    this.timeout = setInterval(() => {
+      let currentIdx = this.state.textIdx;
+      this.setState({ textIdx: currentIdx + 1 });
+    }, 2500);
+  }
+
+  componentDidUnmount() {
+    clearInterval(this.timeout);
+  }
+
+  render() {
+    let skillset = skillsetList[this.state.textIdx % skillsetList.length];
+
+    return (
+      <section class="background-color-black">
+        <div class="container container--medium">
+          <h2 class="about-text color-white margin-0 font-weight-normal">
+            My name is <strong id="nicholas">Nicholas&nbsp;Diesslin</strong>,<br/>
+            I’m a <strong>{skillset}</strong> from <strong>Minnesota</strong>. I like to build, make, and think about things!
+          </h2>
+        </div>
+      </section>
+    )
+  }
 }
 
-export default About
+export default About;
